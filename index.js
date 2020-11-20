5dab2b02cfcecd28a9c6dd680849e6488dc500ef
babaoui <73282716+babaoui@users.noreply.github.com>
2020-11-20 15:15:22 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+var t = document.getElementById("t");
+var b = document.getElementById("b");
+var r = document.getElementById("r");
+
+b.addEventListener('click', () => {
+	var s = t.value.trim();
+	var n = eval(s.replace(/(\w+)(?=\((.+)?\))/g, 'Math.$1'))
+	if(n >= 0) r.innerHTML = s;
+	else {
+		var x = s.match(/[\+\-\*\/]/g);
+		var y = s.split(/[\+\-\*\/]/), z = [];
+		z.push(s[0] == '-' ? '+' : '-');
+		for(let i = 0; i < x.length; ++i) {
+			z.push(y[i]);
+			z.push(x[i] == '+' ? '-' : x[i] == '-' ? '+' : x[i]);
+		}
+		z.push(y[y.length-1]);
+		r.innerHTML = z.join(' ');
+	}
+})
